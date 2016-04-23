6b4036bce55ddedda5bbb35e0275adcdf0ac8ae6
Emily <ebosakowski@gmail.com>
2016-04-23 14:55:13 -0500

    Add procedure seeds, procedure controller, show view, procedures react component

---
+++ app/assets/javascripts/components/procedure.js.jsx
@@ -0,0 +1,22 @@
+var Procedure = React.createClass({
+
+  render: function() {
+    return <div>:D</div>;
+  }
+});
+
+
+// var @Procedure = React.createClass({
+//   // propTypes: {
+
+//   // }
+
+//   render: function() {
+//     return (
+//       <div class="procedure-card">
+//         <h2>{ @procedure.props.term }</h2>
+//         <p>world</p>
+//       </div>
+//       );
+//   }
+// });
