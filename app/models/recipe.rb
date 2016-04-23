89a428bbaca03d1fd160d5fc4d9b0da379f9b7c3
Mollie Stein <mastein109@gmail.com>
2016-04-23 18:51:55 -0500

    Fix travis conflict

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,4 @@
 class Recipe < ActiveRecord::Base
-  searchkick autocomplete: [:name]
-
   has_many :recipe_procedures
   has_many :procedures, through: :recipe_procedures, source: :procedure
   has_many :ingredients
