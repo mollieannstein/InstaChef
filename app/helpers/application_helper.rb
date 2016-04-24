8efb3a9634a9ab868f7e1a7c5a23fb212b8141c6
Luis Ybarra <lrybarra@gmail.com>
2016-04-24 13:36:27 -0500

    highlight text & convert servings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def highlight(text, match_array)
+    stuff = []
+    match_array.each do |m|
+      stuff << Procedure.find(m.procedure_id).term
+    end
+    stuff.each do |s|
+      p s
+      if text.include?(s)
+        text = text.gsub(s, "<mark>"+s+"</mark>")
+      elsif text.downcase.include?(s.downcase)
+        text = text.downcase.gsub(s.downcase, "<mark>"+s.downcase.capitalize+"</mark>")
+      end
+    end
+    return text
+  end
+
+  def recipe_converter(ingredient, converter)
+    return (ingredient.to_r * converter).to_f
+  end
 end
