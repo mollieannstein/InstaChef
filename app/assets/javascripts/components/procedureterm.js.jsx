7197f6990f1db2138f170baf7f6293c500e86c83
Mollie Stein <mastein109@gmail.com>
2016-04-25 14:37:45 -0500

    Fix buttons on homepage

--- app/assets/javascripts/components/procedureterm.js.jsx
+++ app/assets/javascripts/components/procedureterm.js.jsx
@@ -8,7 +8,7 @@ var ProcedureTerm = React.createClass ({
   render: function() {
     return (
       <div onClick={this.handleClick} className="col-sm-3 btn-default panel-heading">
-        {this.props.procedure.term} <div className="panel-body instructions">{this.props.procedure.instructions}</div>
+        {this.props.procedure.term} <div className="button panel-body instructions">{this.props.procedure.instructions}</div>
       </div>
     );
   }
