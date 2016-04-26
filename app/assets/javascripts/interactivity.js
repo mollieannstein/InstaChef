853a70955f07988170453483695fb841cc319ebe
Emily <ebosakowski@gmail.com>
2016-04-26 09:22:47 -0500

    Finish autosuggest search bar

--- app/assets/javascripts/interactivity.js
+++ app/assets/javascripts/interactivity.js
@@ -1,4 +1,24 @@
+function recipeSearch(input) {
+  $(".drop-down").remove();
+  $.ajax({
+    url: "/recipes",
+    data: input,
+  }).done(function(recipeItemPartial) {
+    $(recipeItemPartial).appendTo(".main-searchbar");
+  });
+}
+
+var throttledSearch = _.throttle(recipeSearch, 300);
+
 $( document ).ready(function() {
     // console.log( "ready!" );
-    $().on()
+    $(".form-control").keyup(function(event){
+      if (event.keyCode != 13) {
+        // console.log(event.keyCode);
+        event.preventDefault();
+        var input = $(this).serialize();
+        throttledSearch(input);
+      }
+
+    });
 });
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,15 @@
 class RecipesController < ApplicationController
   # autocomplete :recipe, :name
 
+
+# if user presses "enter", it's processed like a normal http request, otherwise it's an ajax
   def index
-    @recipes = Recipe.all
+    # puts "I'm here"
     if params[:search]
       @recipes = Recipe.search(params[:search])
+      if request.xhr?
+        render partial: "search", locals: { recipes: @recipes, search: params[:search]}
+      end
     else
       @recipes = Recipe.all
     end
