a2b546fdaa01659cc36959eefb8d9e468554a9fb
Emily <ebosakowski@gmail.com>
2016-04-26 14:52:53 -0500

    Add routes and controller paths for conversion and temp charts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
+
+  def temp
+    render :placeholder
+  end
+
+  def conversion
+    render :placeholder
+  end
+
   def index
     @procedures = Procedure.all.sort_count_frequency
     render :index
