26137823a994d1c71543b9f23f84f0654a6f503b
Mollie Stein <mastein109@gmail.com>
2016-04-26 09:02:04 -0500

    Remove api keys and add to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ require 'nokogiri'
 require 'verbs'
 
 ############# REMOVE BEFORE PUSH ###########
-APP_KEY = ""
-APP_ID = ""
+APP_KEY = ENV['API_KEY']
+APP_ID = ENV['YUMMLY_ID']
 
-YOUTUBE_API = ""
+YOUTUBE_API = ENV['YOUTUBE_API']
 
 
 yummly_rId_array = ["Chicken-And-Dumplings-I-Allrecipes", "Salsa-Allrecipes", "Horseradish-Sauce-Allrecipes_1", "Snow-Peak-Frosting-Allrecipes", "Classic-Candied-Sweet-Potatoes-Allrecipes", "Butterscotch-Drops-Allrecipes", "Tropical-Grilled-Chicken-Breast-AllRecipes-39303", "Bannock-Allrecipes", "Good-Old-Fashioned-Pancakes-546169", "Best-Spanish-Rice-Allrecipes"]
