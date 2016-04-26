ba6fb6c744641540bad1d3b97890fd4e565e06ee
Emily <ebosakowski@gmail.com>
2016-04-26 14:00:38 -0500

    Add procedures index page with partial

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -2,8 +2,9 @@ class ProceduresController < ApplicationController
 
   # route to hit
   # partial is called _procedure_index
+  # take sort_count_frequency off if you don't want to sort by frequency
   def index
-    @procedures = Procedure.all
+    @procedures = Procedure.all.sort_count_frequency
   end
 
   def show
