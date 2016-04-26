06e04d132db3a766402d28560a84d85d43554a5b
Emily <ebosakowski@gmail.com>
2016-04-26 13:52:42 -0500

    Routes, controller, start partial

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,5 +1,11 @@
 class ProceduresController < ApplicationController
 
+  # route to hit
+  # partial is called _procedure_index
+  def index
+    @procedures = Procedure.all
+  end
+
   def show
     @procedure = Procedure.find(params[:id])
   end
