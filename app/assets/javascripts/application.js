74b8f8adcdb1064903b0955623da9829e83f7ce6
Ian Wudarski <email": null>
2016-04-27 11:27:24 -0500

    fix JS appends

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,10 +101,8 @@ $(document).ready(function(){
 
   //slide toggle conversions from menu bar
   $('#myNavbar').on('click', '.conversions', function(event){
-    $('.conversions').append($('.liquid_converter_container'));
     $('.fluid_converter').slideToggle("slow");
 
-    $('.conversions').append($('.weight_converter_container'));
     $('.weight_converter').slideToggle("slow");
     event.preventDefault();
   });
