e09063137870118a6cac952b092aa53d56a16c09
Ian Wudarski <email": null>
2016-04-27 16:10:51 -0500

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ require 'verbs'
 
 ############# REMOVE BEFORE PUSH ###########
 
-APP_KEY = '3426a1046705ec18632e7c1386fad156' #ENV['API_KEY']
-APP_ID = 'e512ae51' #ENV['YUMMLY_ID']
-YOUTUBE_API = 'AIzaSyAJvghM4FRJ4pHZ0jcMPh7gdMFIjmyKDnI' #ENV['YOUTUBE_API']
+APP_KEY = ENV['API_KEY']
+APP_ID = ENV['YUMMLY_ID']
+YOUTUBE_API = ENV['YOUTUBE_API']
 
 
 yummly_rId_array = ["Chicken-And-Dumplings-I-Allrecipes", "Salsa-Allrecipes", "Horseradish-Sauce-Allrecipes_1", "Snow-Peak-Frosting-Allrecipes", "Classic-Candied-Sweet-Potatoes-Allrecipes", "Butterscotch-Drops-Allrecipes", "Tropical-Grilled-Chicken-Breast-AllRecipes-39303", "Bannock-Allrecipes", "Good-Old-Fashioned-Pancakes-546169", "Best-Spanish-Rice-Allrecipes"]
