55c944c06253319e38f0d8c9c3e59766de6ca517
Emily <ebosakowski@gmail.com>
2016-04-28 08:41:36 -0500

    Write first model tests

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Recipe do
-  let(:recipe) { Recipe.new(name: "Mac and Cheese")}
+  let(:recipe) { Recipe.create(name: "Mac and Cheese")}
 
   it "creates a recipe with a name 'Mac and Cheese'" do
     expect(recipe.name).to eq("Mac and Cheese")
   end
+
+  # it "finds recipes in the database whose names match a query string" do
+  #   expect(Recipe.all.search("Mac")).to match(recipe)
+  # end
+
 end
